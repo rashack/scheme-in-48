50a24e64c896f1ceb018987eb3fbb929bc37c71e
Kjell Andreassen <andreassen.kjell@gmail.com>
2016-10-29 23:57:09 +0200

    Remove old exercise examples

--- exercise2-2.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-import System.Environment
-
-rInt :: String -> Int
-rInt = read
-
-main :: IO ()
-main = do args <- getArgs
-          putStrLn ("The sum is " ++ show ((rInt $ args !! 0) + (rInt  $ args !! 1)))

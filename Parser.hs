9fd740c64055a80b7170ecb69d93374e57ea150a
Kjell Andreassen <andreassen.kjell@gmail.com>
2016-10-29 13:15:39 +0200

    Moved a little code around

--- Parser.hs
+++ Parser.hs
@@ -24,6 +24,15 @@ data LispVal = Atom String
              | Bool Bool
   deriving (Eq, Show)
 
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = [first] ++ rest
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          otherwise -> Atom atom
+
 parseList :: Parser LispVal
 parseList = liftM List $ sepBy parseExpr spaces
 
@@ -38,49 +47,6 @@ parseQuoted = do
     char '\''
     x <- parseExpr
     return $ List [Atom "quote", x]
-
-parseString :: Parser LispVal
-parseString = do char '"'
-                 x <- many parseStrStr
-                 char '"'
-                 return $ String $ concat x
-
-parseStrStr :: Parser String
-parseStrStr = escapedChar
-          <|> normalChar
-
-normalChar :: Parser String
-normalChar = do
-  c <- noneOf "\\\""
-  return [c]
-
-escapedChar :: Parser String
-escapedChar = do
-  char '\\'
-  c <- oneOf escapables
-  return [chr $ c2i c]
-
-escapables = "0abtnvfr\"\\"
-
-c2i '0'  =  0 -- NUL (null)
-c2i 'a'  =  7 -- BEL (bell)
-c2i 'b'  =  8 -- BS  (backspace)
-c2i 't'  =  9 -- HT  (horizontal tab)
-c2i 'n'  = 10 -- LF  (new line)
-c2i 'v'  = 11 -- VT  (vertical tab)
-c2i 'f'  = 12 -- FF  (form feed)
-c2i 'r'  = 13 -- CR  (carriage return)
-c2i '\"' = 34
-c2i '\\' = 92
-
-parseAtom :: Parser LispVal
-parseAtom = do first <- letter <|> symbol
-               rest <- many (letter <|> digit <|> symbol)
-               let atom = [first] ++ rest
-               return $ case atom of
-                          "#t" -> Bool True
-                          "#f" -> Bool False
-                          otherwise -> Atom atom
 
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
@@ -116,6 +82,40 @@ readBin = do
   let (int, _) = foldr (\d (sum, e) -> (sum + (ord d - 48) * e, shift e 1)) (0, 1) ds
   return $ Number $ toInteger int
 
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many parseStrStr
+                 char '"'
+                 return $ String $ concat x
+
+parseStrStr :: Parser String
+parseStrStr = escapedChar
+          <|> normalChar
+
+normalChar :: Parser String
+normalChar = do
+  c <- noneOf "\\\""
+  return [c]
+
+escapedChar :: Parser String
+escapedChar = do
+  char '\\'
+  c <- oneOf escapables
+  return [chr $ c2i c]
+
+escapables = "0abtnvfr\"\\"
+
+c2i '0'  =  0 -- NUL (null)
+c2i 'a'  =  7 -- BEL (bell)
+c2i 'b'  =  8 -- BS  (backspace)
+c2i 't'  =  9 -- HT  (horizontal tab)
+c2i 'n'  = 10 -- LF  (new line)
+c2i 'v'  = 11 -- VT  (vertical tab)
+c2i 'f'  = 12 -- FF  (form feed)
+c2i 'r'  = 13 -- CR  (carriage return)
+c2i '\"' = 34
+c2i '\\' = 92
+
 parseExpr :: Parser LispVal
 parseExpr = try parseRadixNumber
         <|> parseAtom
@@ -126,11 +126,6 @@ parseExpr = try parseRadixNumber
                x <- try parseList <|> parseDottedList
                char ')'
                return x
-
-readExpr :: String -> String
-readExpr input = case parse parseExpr "lisp" input of
-                   Left err -> "No match: " ++ show err
-                   Right val -> "Found value: " ++ show val
 
 
 -- quickcheck --
--- listing3-3.hs
+++ listing3-3.hs
@@ -2,7 +2,13 @@ module Main where
 
 import Parser
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
 main = do args <- getArgs
           putStrLn (readExpr (args !! 0))
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+                   Left err -> "No match: " ++ show err
+                   Right val -> "Found value: " ++ show val
